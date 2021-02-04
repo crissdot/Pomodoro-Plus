2c93b8f7845c8bb124f1ff5e99c42908a9018fb6
crissdot <cris-2-11-2001@hotmail.com>
2021-02-04 09:27:42 -0600

    refactor again timer

--- src/js/Timer.js
+++ src/js/Timer.js
@@ -4,20 +4,44 @@ class Timer {
         this.hours = timer.time.hours || 0;
         this.minutes = timer.time.minutes || 0;
         this.seconds = timer.time.seconds || 0;
+        this.interval;
+        this.productivityTime = timer.productivityTime;
+        this.productivityTotal = timer.productivityTotal;
     }
 
     start() {
-        setInterval(() => {
+        this.interval = setInterval(() => {
             this._formatTimer();
-            this.timerCounter.innerHTML = `${this.minutes}:${this.seconds}`;
+            this.timerCounter.innerHTML = `${this.minutes}:${this.seconds}`; // TODO add rest class
         }, 1000);
     }
 
+    finish() {
+        clearInterval(this.interval);
+        this._addProductivityTime();
+        this._resetTimer();
+    }
+
+    _resetTimer() {
+        this.hours = 0;
+        this.minutes = 0;
+        this.seconds = 0; // TODO remove rest class
+        this.timerCounter.innerHTML = '0:0';
+    }
+
     _formatTimer() {
-        if(this.seconds === 59) {
+        if(this.seconds === 1) {
             this.minutes++;
             this.seconds = 0;
         } else this.seconds++;
+    }
+
+    _addProductivityTime() {
+        if(this.minutes >= 5 || this.hours > 0) {
+            const productivityTime = document.createElement('p');
+            productivityTime.append(`${this.minutes}:${this.seconds}`.toString());
+            this.productivityTime.appendChild(productivityTime);
+        }
     }
 }
 
--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ const btnStart = document.querySelector('.timer__btn--start');
 const btnAgain = document.querySelector('.timer__btn--again');
 const btnStop = document.querySelector('.timer__btn--stop');
 const productivityTime = document.querySelector('.productivity__time');
-const total = document.querySelector('.productivity__total');
+const productivityTotal = document.querySelector('.productivity__total');
 
 const timerCounter = {
     timer: displayTimer,
@@ -14,6 +14,8 @@ const timerCounter = {
         minutes: 0,
         seconds: 0,
     },
+    productivityTime,
+    productivityTotal,
 }
 
 const timer = new Timer(timerCounter);
@@ -37,31 +39,33 @@ btnStart.addEventListener('click', () => {
 });
 
 btnAgain.addEventListener('click', () => {
-    clearInterval(runTimer);
-    if(timer.minutes >= 5) {
-        const p = document.createElement('p');
-        const text = document.createTextNode(`${timer.minutes}:${timer.seconds}`);
-        p.appendChild(text);
-        productivityTime.appendChild(p);
-    }
-    timer = {
-        minutes: 0,
-        seconds: 0,
-    };
-    displayTimer.classList.remove('timer__counter--rest');
-    displayTimer.innerHTML = '0:0';
+    // clearInterval(runTimer);
+    // if(timer.minutes >= 5) {
+    //     const p = document.createElement('p');
+    //     const text = document.createTextNode(`${timer.minutes}:${timer.seconds}`);
+    //     p.appendChild(text);
+    //     productivityTime.appendChild(p);
+    // }
+    // timer = {
+    //     minutes: 0,
+    //     seconds: 0,
+    // };
+    // displayTimer.classList.remove('timer__counter--rest');
+    // displayTimer.innerHTML = '0:0';
 
-    const allTime = document.querySelectorAll('.productivity__time > p');
-    let totalTime = 0;
-    allTime.forEach(p => {
-        const time = p.innerHTML.split(':');
-        totalTime += time[0]*60 + parseInt(time[1]);
-    });
-    for(var i = 0; totalTime >= 60; i++) {
-        totalTime -= 60;
-    }
-    let min = i;
-    total.innerHTML = `Total: ${min}:${totalTime}`;
+    timer.finish();
+
+    // const allTime = document.querySelectorAll('.productivity__time > p');
+    // let totalTime = 0;
+    // allTime.forEach(p => {
+    //     const time = p.innerHTML.split(':');
+    //     totalTime += time[0]*60 + parseInt(time[1]);
+    // });
+    // for(var i = 0; totalTime >= 60; i++) {
+    //     totalTime -= 60;
+    // }
+    // let min = i;
+    // total.innerHTML = `Total: ${min}:${totalTime}`;
 })
 
 btnStop.addEventListener('click', () => {
