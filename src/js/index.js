808407a729472f046a8becd562be5e6f45814cc7
crissdot <cris-2-11-2001@hotmail.com>
2021-02-13 17:42:45 -0600

    add disabled attr in buttons

--- src/js/index.js
+++ src/js/index.js
@@ -28,21 +28,29 @@ btnFinish.addEventListener('click', () => {
     btnStart.addEventListener('click', startHandler);
     timer.finish();
     btnStart.style.opacity = 1;
+    btnStart.disabled = false;
     btnPause.style.opacity = 0;
+    btnPause.disabled = true;
     btnFinish.style.opacity = 0;
+    btnFinish.disabled = true;
 });
 
 btnPause.addEventListener('click', () => {
     btnStart.addEventListener('click', startHandler);
     timer.pause();
     btnStart.style.opacity = 1;
+    btnStart.disabled = false;
     btnPause.style.opacity = 0;
+    btnPause.disabled = true;
 });
 
 function startHandler() {
     btnStart.removeEventListener('click', startHandler);
     timer.start();
     btnStart.style.opacity = 0;
+    btnStart.disabled = true;
     btnPause.style.opacity = 1;
+    btnPause.disabled = false;
     btnFinish.style.opacity = 1;
+    btnFinish.disabled = false;
 }
