7cc4cdd829095691d1f5628ef54b5bf4c7c6fb85
crissdot <cris-2-11-2001@hotmail.com>
2021-04-10 19:06:10 -0500

    add audio button style

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -3,6 +3,19 @@ import Timer from './CountTimer.js';
 import TimerUp from './CountUpTimer.js';
 import TimerDown from './CountDownTimer.js';
 
+const soundCheckElement = document.querySelector('#toggle--sound');
+const volumeOn = document.querySelector('.timer__volume-on');
+const volumeOff = document.querySelector('.timer__volume-off');
+
+soundCheckElement.addEventListener('change', () => {
+    if(soundCheckElement.checked) {
+        volumeOff.style.opacity = 0;
+        volumeOn.style.opacity = 1;
+    } else {
+        volumeOn.style.opacity = 0;
+        volumeOff.style.opacity = 1;
+    }
+})
 
 function makeTimerUp() {
     const countUpTimer = {
